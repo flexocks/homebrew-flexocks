594665b5722d5e8df0ef3d308bd17bce5028a5b9
Rodrigo <rodrigo@Rodrigos-MBP.fritz.box>
2023-09-09 22:21:11 +0200

    Cambiando la formula

--- Formula/flexocks.rb
+++ Formula/flexocks.rb
@@ -1,7 +1,7 @@
 class Flexocks < Formula
   desc "Conexion Proxy SOCKS configurable"
   homepage "https://github.com/flexocks/homebrew-flexocks"
-  url "https://github.com/flexocks/homebrew-flexocks/flexocks.pkg" 
+  url "https://github.com/flexocks/homebrew-flexocks/blob/main/flexocks.pkg" 
   sha256 "656f91ef4a121d2a780fd031a427484c423c93858755ccd9a4c5d00bbd0a1dd9"
   version "beta rc1"
 
