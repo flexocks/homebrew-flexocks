9a47b7a21672fe79e727dfe9c44c25a17dba007e
Rodrigo <rodrigo@Rodrigos-MBP.fritz.box>
2023-09-09 23:10:25 +0200

    nuevo

--- Formula/flexocks.rb
+++ Formula/flexocks.rb
@@ -2,8 +2,8 @@ class Flexocks < Formula
   desc "Conexion Proxy SOCKS configurable"
   homepage "https://github.com/flexocks/homebrew-flexocks"
   url "https://github.com/flexocks/homebrew-flexocks/raw/main/flexocks.pkg" 
-  sha256 "f8ebba6213760cb45aa9de26b48cbbb51c0b4137ffb870a8bd64223fe5bab5d8"
-  version "beta-rc1"
+  sha256 "e0224a82b1d7a055758a4611bd25c77688cf977b1f312519d5b8d048f316a78e"
+  version "beta"
 
   def install
     system "sudo", "installer", "-pkg", "Flexocks.pkg", "-target", "/"
