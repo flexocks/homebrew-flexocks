a6e7b2a22572c0690da962e619950667c9ff1150
Rodrigo <rodrigo@Rodrigos-MBP.fritz.box>
2023-09-09 23:02:23 +0200

    version

--- Formula/flexocks.rb
+++ Formula/flexocks.rb
@@ -3,7 +3,7 @@ class Flexocks < Formula
   homepage "https://github.com/flexocks/homebrew-flexocks"
   url "https://github.com/flexocks/homebrew-flexocks/raw/main/flexocks.pkg" 
   sha256 "656f91ef4a121d2a780fd031a427484c423c93858755ccd9a4c5d00bbd0a1dd9"
-  version "beta rc1"
+  version "beta-rc1"
 
   def install
     system "sudo", "installer", "-pkg", "Flexocks.pkg", "-target", "/"
